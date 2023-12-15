a338f6df3e57ac92410367d1964022d5177e212b
Mykhailo <sharkon90@gmail.com>
2023-12-14 22:46:43 -0500

    adding functions for promp and function to crate files depending og the promp

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is title name of your project?",
+    "Provide a short description explaining why did you build this project?",
+    "What problem does it solve?",
+    "What was your motivation?",
+    "What are the steps required to install your project?",
+    "Provide instructions and examples for use",
+    "List your collaborators, if any",
+    "Add your test instructions",
+    "What is good email to reach you with additional questions?",
+    "What is your GitHub username?",
+    "What license do you want?",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+const generatedFile = generateMarkdown(data);
+// console.log(generatedFile);
+fs.writeFile(`${fileName}.md`, generatedFile, (err) => err ? console.log("something wrong") : console.log('succseed!'));
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "license",
+            message: questions[10],
+            choices: ["none", "MIT", "GPLv2"],
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0]
+        },
+        {
+            type: 'input',
+            name: 'projectWhy',
+            message: questions[1]
+        },
+        {
+            type: 'input',
+            name: 'problemSolve',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'motivation',
+            message: questions[3]
+        },
+        {
+            type: 'input',
+            name: 'installationSteps',
+            message: questions[4]
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'cont',
+            message: questions[6]
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: questions[7]
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[8]
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: questions[9]
+        },
+
+    ]).then((data) => writeToFile("README", data))
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license){
+    if(license === "MIT"){
+      let licenseBadge  = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      return licenseBadge;
+    } 
+    if(license === "GPLv2"){
+      let licenseBadge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+      return licenseBadge;
+    }
+  } else return "";
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,11 +20,101 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license){
+    if(license === "MIT"){
+      let licenseText = `
+      ## License
+      MIT License
 
+      Copyright (c) 2023 MykhailoZakh
+  
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+  
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+  
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.
+      `;
+      return licenseText;
+    }
+    if(license === "GPLv2"){
+      let licenseText = `
+      ## License
+      
+      GNU GENERAL PUBLIC LICENSE
+      Version 2, June 1991
+  
+      Copyright (C) 1989, 1991 Free Software Foundation, Inc.,
+      51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+      Everyone is permitted to copy and distribute verbatim copies
+      of `;
+      return licenseText;
+    }
+  } else return "";
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  ## ${data.title}
+
+  ## Badges
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+
+  ${data.projectWhy}
+
+  ${data.problemSolve}
+  
+  ${data.motivation}
+  
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installationSteps}
+  
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.cont}
+  
+  ## Tests
+
+  ${data.tests}
+
+  ${renderLicenseSection(data.license)}
+  
+  ## Questions
+
+  If you have any question reach me throu email.
+  Email : ${data.email} 
+
+  GitHub Profile:
+  https://github.com/${data.username}
 
 `;
 }
